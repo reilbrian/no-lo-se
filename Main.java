c198be97bf03aa9e1fabc1eeb000eb5aa932931d
brian <brian.elrey2002@gmail.com>
2023-03-09 20:59:26 -0300

    le agregue un mensaje

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("holaaa");
         for (int i=12;i>1;i--){
             if (i % 2 == 0) {
                     System.out.println(i);
