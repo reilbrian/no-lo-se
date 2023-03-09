dc2f0aa98c9901f6b25038f8979904ef02e17a51
reilbrian <126490904+reilbrian@users.noreply.github.com>
2023-03-09 20:34:29 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i=12;i>1;i--){
+            if (i % 2 == 0) {
+                    System.out.println(i);
+            }
+        }
+    }
+
+}
